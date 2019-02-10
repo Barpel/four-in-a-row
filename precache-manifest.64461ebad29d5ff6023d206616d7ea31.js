03fc01d6c186a0c4445a886aee866bd68f5e3bf9
Bar Peled <ibarpeled@gmail.com>
2019-02-11 00:19:49 +0200

    Updates

---
+++ precache-manifest.64461ebad29d5ff6023d206616d7ea31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d460a4d16fd71147c596",
+    "url": "/four-in-a-row/static/css/main.9e5dcdf4.chunk.css"
+  },
+  {
+    "revision": "d460a4d16fd71147c596",
+    "url": "/four-in-a-row/static/js/main.d460a4d1.chunk.js"
+  },
+  {
+    "revision": "8c1d90eb748bd67dfc27",
+    "url": "/four-in-a-row/static/js/1.8c1d90eb.chunk.js"
+  },
+  {
+    "revision": "223aa477ef69b711389a",
+    "url": "/four-in-a-row/static/js/runtime~main.223aa477.js"
+  },
+  {
+    "revision": "02a415f3a09691933c3d732233ef45b8",
+    "url": "/four-in-a-row/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/four-in-a-row/game/precache-manifest.96fa9e741144ccf0d38fd7953ceb0e67.js"
+  "/four-in-a-row/precache-manifest.64461ebad29d5ff6023d206616d7ea31.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/four-in-a-row/game/index.html", {
+workbox.routing.registerNavigationRoute("/four-in-a-row/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
